eaf62f99bb9e8491d046b8860f5b6932a99b912e
michburkhalter <michburkhalter@gmail.com>
2021-06-04 13:27:43 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-import React, { Component } from 'react'
-import "./style.css";
-import QrReader from 'react-qr-scanner'
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import QrReader from 'react-qr-scanner';
 
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       delay: 100,
       result: 'No result',
-      facingmode: 'user'
-    }
+      facingmode: 'rear'
+    };
 
-    this.handleScan = this.handleScan.bind(this)
+    this.handleScan = this.handleScan.bind(this);
+    this.switchCamera = this.switchCamera.bind(this);
   }
-  handleScan(data){
+  handleScan(data) {
     this.setState({
-      result: data,
-    })
+      result: data
+    });
   }
-  handleError(err){
-    console.error(err)
+  handleError(err) {
+    console.error(err);
   }
-  render(){
+  switchCamera() {
+    if (this.state.facingmode == 'rear') {
+      this.setState({
+        facingmode: 'front'
+      });
+      console.log('front');
+    }else if (this.state.facingmode == 'front'){
+      this.setState({
+        facingmode: 'rear'
+      });
+      console.log('rear')
+    }
+  }
+  render() {
     const previewStyle = {
       height: 240,
-      width: 320,
-    }
+      width: 320
+    };
 
-    return(
+    return (
       <div>
         <QrReader
           delay={this.state.delay}
@@ -36,13 +52,15 @@ class App extends Component {
           onError={this.handleError}
           onScan={this.handleScan}
           facingMode={this.state.facingmode}
-          />
+        />
+        <p></p>
+        <Button variant="primary" onClick={this.switchCamera}>
+          Switch Camera
+        </Button>
         <p>{this.state.result}</p>
       </div>
-    )
+    );
   }
 }
-
-
 
 export default App;
